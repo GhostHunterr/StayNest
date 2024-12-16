e2a2871db6549bba27967696731200e25fc577d8
Farhan Sarang <farhansarang7@gmail.com>
2024-12-16 08:10:59 -0500

    Implemented Udpate and Delete Functionalities.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const app = express();
 const PORT = 8080;
@@ -10,6 +11,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/staynest";
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
     .then(() => {
@@ -61,11 +63,45 @@ app.get("/listings/:id", async (req, res) => {
 
 //Create Route
 app.post("/listings", async (req, res) => {
-    
+
     //sending data as a key-value pair.
     const newListing = new Listing(req.body.listing);
     try {
         await newListing.save();
+        res.redirect("/listings");
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const listing = await Listing.findById(id);
+        res.render("listings/edit.ejs", { listing });
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+        res.redirect(`/listings/${id}`);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Listing.findByIdAndDelete(id);
         res.redirect("/listings");
     } catch (err) {
         res.send(err);
