8f41d8b6564f7ec201fe3de9948f6a73c6019ea3
Farhan Sarang <farhansarang7@gmail.com>
2024-12-16 08:45:19 -0500

    Created boilerplate for the website

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require('ejs-mate');
 
 const app = express();
 const PORT = 8080;
@@ -12,6 +13,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
 
 main()
     .then(() => {
